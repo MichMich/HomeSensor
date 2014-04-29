8cb48b9946866fdd7bb855de306499a7d67db1d1
Michael Teeuw <michael@xonaymedia.nl>
2014-04-29 21:31:16 +0200

    Change expiry

--- Node/PushNotifier.js
+++ Node/PushNotifier.js
@@ -23,9 +23,8 @@ function PushNotifier() {
 		var myDevice = new apn.Device(deviceToken);
 		var notification = new apn.Notification();
 
-		notification.expiry = Math.floor(Date.now() / 1000) + 3600 * 24; // Expires 1 hour from now.
 		notification.badge = 0;
-		notification.sound = (sound) ? sound : "ping.aiff";
+		notification.sound = (sound) ? sound : "default";
 		notification.alert = message;
 		notification.payload = {};
 
